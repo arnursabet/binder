08dfc209f67f28914fe0b20c2d80c6fc391933d7
chrislee03 <chrislee03@berkeley.edu>
2024-11-11 17:16:29 -0800

    initialized SwiftUI

---
+++ Binder/Binder/BinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinderApp.swift
+//  Binder
+//
+//  Created by Chris Lee on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Binder/BinderTests/BinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  BinderTests.swift
+//  BinderTests
+//
+//  Created by Chris Lee on 11/11/24.
+//
+
+import Testing
+@testable import Binder
+
+struct BinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
